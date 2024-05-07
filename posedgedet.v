e4a30cb65bccb4cb737281785d6185d96dbcbed7
Atmadip <52615332+Atmadip@users.noreply.github.com>
2024-05-06 23:45:41 -0500

    Update posedgedet.v

--- posedgedet.v
+++ posedgedet.v
@@ -4,37 +4,22 @@ module model (
   input din,
   output dout
 );
-// parameter reset = 0, state_1 = 1, state_2 = 2;
-// reg [1:0] state;
-// always @(posedge clk) begin
-// if(~resetn)
-//   state <= reset;
-// else begin
-//   case(state)
-//     reset : state <= (din ? state_1 : reset);
-//     state_1:state <= (din ? state_2 : reset);
-//     state_2:state <= (din ? state_2 : reset);
-//     default: state<= reset;
-//   endcase
-// end
-// end
-// assign dout = (state == state_1) ? 0:0;
 
 reg temp, temp_1;
 always @(posedge clk)
 begin
   if (~resetn)
-    temp = 0;
+    temp <= 0;
   else
-    temp = din;
+    temp <= din;
 end
 
 always @(posedge clk)
 begin
   if (~resetn)
-    temp_1 = 0;
+    temp_1 <= 0;
   else
-    temp_1 = temp;
+    temp_1 <= temp;
 end
 
 assign dout = (~temp_1) & temp;
